24686273c9dab157484a59eac3af85ccf16becb2
Tom Gehrke <phyrog@cognitive-coding.com>
2014-04-16 21:49:48 +0200

    Add tests for fuzzy searching

--- app/models/torrent.rb
+++ app/models/torrent.rb
@@ -13,8 +13,12 @@ class Torrent < ActiveRecord::Base
 
   def size
     h = self.to_h
-    @size ||= h["info"]["length"] if not h["info"]["files"]
-    @size ||= h["info"]["files"].map { |f| f["length"] }.reduce(:+)
+    if h
+      @size ||= h["info"]["length"] if not h["info"]["files"]
+      @size ||= h["info"]["files"].map { |f| f["length"] }.reduce(:+)
+    else
+      0
+    end
   end
 
   def formatted_size
@@ -23,48 +27,60 @@ class Torrent < ActiveRecord::Base
 
   def no_files
     h = self.to_h
-    if h["info"]["files"]
-      h["info"]["files"].length
+    if h
+      if h["info"]["files"]
+        h["info"]["files"].length
+      else
+        1
+      end
     else
-      1
+      0
     end
   end
 
   def list_of_files
     h = self.to_h
-    if h["info"]["files"]
-      h["info"]["files"].map do |file|
-        File.join(h["info"]["name"], file["path"])
+    if h
+      if h["info"]["files"]
+        h["info"]["files"].map do |file|
+          File.join(h["info"]["name"], file["path"])
+        end
+      else
+        [h["info"]["name"]]
       end
     else
-      [h["info"]["name"]]
+      []
     end
   end
 
   def tree_of_files
     h = self.to_h
-    if h["info"]["files"]
-      tree = [{ label: h["info"]["name"], children: [] }]
-      h["info"]["files"].map do |file|
-        t = tree
-        file["path"].unshift(h["info"]["name"]).each do |path|
-          c = t.detect { |p| p[:label] == path }
-          if c
-            t = c[:children]
-          else
-            if path == file["path"].last
-              t << { label: path, length: file["length"] }
-              t = t.detect { |p| p[:label] == path }
+    if h
+      if h["info"]["files"]
+        tree = [{ label: h["info"]["name"], children: [] }]
+        h["info"]["files"].map do |file|
+          t = tree
+          file["path"].unshift(h["info"]["name"]).each do |path|
+            c = t.detect { |p| p[:label] == path }
+            if c
+              t = c[:children]
             else
-              t << { label: path, children: [] }
-              t = t.detect { |p| p[:label] == path }[:children]
+              if path == file["path"].last
+                t << { label: path, length: file["length"] }
+                t = t.detect { |p| p[:label] == path }
+              else
+                t << { label: path, children: [] }
+                t = t.detect { |p| p[:label] == path }[:children]
+              end
             end
           end
         end
+      else
+        tree = [{ label: h["info"]["name"], length: h["info"]["length"] }]
       end
+      tree
     else
-      tree = [{ label: h["info"]["name"], length: h["info"]["length"] }]
+      []
     end
-    tree
   end
 end
---
+++ features/fuzzy_search.feature
@@ -0,0 +1,32 @@
+Feature: Fuzzy search
+  In order to find a torrent
+  As a user
+  I want to search for some words that are similar to what I am looking for
+
+  Scenario: I want to search for a word appearing in the name
+    Given there is a torrent with name: "Some awesome torrent" and description: "This description is really nice too!"
+    When I visit the page
+    And I enter into the search bar: "awesome"
+    And I click the button "Search"
+    Then I can see the torrent
+
+  Scenario: I want to search for a word appearing in the description
+    Given there is a torrent with name: "Some awesome torrent" and description: "This description is really nice too!"
+    When I visit the page
+    And I enter into the search bar: "nice"
+    And I click the button "Search"
+    Then I can see the torrent
+
+  Scenario: I want to search for a word that is spelled incorrectly
+    Given there is a torrent with name: "Some awesome torrent" and description: "This description is really nice too!"
+    When I visit the page
+    And I enter into the search bar: "discrebton"
+    And I click the button "Search"
+    Then I can see the torrent
+
+  Scenario: I want to search for a word that matches no torrent
+    Given there is a torrent with name: "Some awesome torrent" and description: "This description is really nice too!"
+    When I visit the page
+    And I enter into the search bar: "fabulous"
+    And I click the button "Search"
+    Then I can't see the torrent
