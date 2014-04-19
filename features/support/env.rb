a9e07fb7660d8c5167800b6f3cae3d6490eb73cd
Tom Gehrke <phyrog@gmail.com>
2014-04-19 21:04:29 +0200

    Add coveralls

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,9 @@
 
 require 'cucumber/rails'
 require 'simplecov'
+require 'coveralls'
 SimpleCov.start
+Coveralls.wear!
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
