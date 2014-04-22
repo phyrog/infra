9fcbfc12823395c0c4bafab9a0e3484f4509578e
Tom Gehrke <phyrog@cognitive-coding.com>
2014-04-22 14:19:21 +0200

    Make property values typed

--- app/models/property_value.rb
+++ app/models/property_value.rb
@@ -2,6 +2,15 @@ class PropertyValue < ActiveRecord::Base
   belongs_to :property
   belongs_to :torrent
 
+  def typed_value
+    case self.property.value_type
+      when 'Integer'
+        self.value.to_i
+      else
+        self.value
+    end
+  end
+
   def to_s
     self.value.to_s
   end
--- db/migrate/20140416232824_create_properties.rb
+++ db/migrate/20140416232824_create_properties.rb
@@ -2,6 +2,7 @@ class CreateProperties < ActiveRecord::Migration
   def change
     create_table :properties do |t|
       t.string :name
+      t.string :value_type
       t.string :postfix
     end
   end
