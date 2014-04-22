a604a29c10d213d8e92fe1cc4ad7749d369e3825
Tom Gehrke <phyrog@cognitive-coding.com>
2014-04-22 14:19:21 +0200

    Make property values typed

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20140416233335) do
 
   create_table "properties", force: true do |t|
     t.string "name"
+    t.string "value_type"
     t.string "postfix"
   end
 
@@ -55,16 +56,5 @@ ActiveRecord::Schema.define(version: 20140416233335) do
     t.string   "file_uid"
     t.string   "file_name"
   end
-
-  create_table "trigrams", force: true do |t|
-    t.string  "trigram",     limit: 3
-    t.integer "score",       limit: 2
-    t.integer "owner_id"
-    t.string  "owner_type"
-    t.string  "fuzzy_field"
-  end
-
-  add_index "trigrams", ["owner_id", "owner_type", "fuzzy_field", "trigram", "score"], name: "index_for_match"
-  add_index "trigrams", ["owner_id", "owner_type"], name: "index_by_owner"
 
 end
