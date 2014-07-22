5f7c50e68587ab0d74a06ae8b4ff1108c99a57e4
Tom Gehrke <phyrog@cognitive-coding.com>
2014-07-22 21:12:47 +0200

    Add missing db schema for trigrams

--- db/schema.rb
+++ db/schema.rb
@@ -57,4 +57,15 @@ ActiveRecord::Schema.define(version: 20140416233335) do
     t.string   "file_name"
   end
 
+  create_table "trigrams", force: true do |t|
+    t.string  "trigram",     limit: 3
+    t.integer "score",       limit: 2
+    t.integer "owner_id"
+    t.string  "owner_type"
+    t.string  "fuzzy_field"
+  end
+
+  add_index "trigrams", ["owner_id", "owner_type", "fuzzy_field", "trigram", "score"], name: "index_for_match"
+  add_index "trigrams", ["owner_id", "owner_type"], name: "index_by_owner"
+
 end
